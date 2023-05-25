d53900fece84b1363880676fa997948b981d71cc
omanias <ojmanias1985@gmail.com>
2023-05-24 22:30:20 -0300

    agrego clase de eventos y dom

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-/* let elemento
-
-elemento= document
+/* let elemento;
+elemento = document
 elemento = document.head
 elemento = document.body
 elemento = document.forms
@@ -9,76 +8,232 @@ elemento = document.images
 
 console.log(elemento) */
 
-// METODO TRADICIONAL => id, class, tags
 
-/* const navbar = document.getElementsByClassName("navbar") 
+/*
+clases
+ids
+tags
+
+*/
+
+//METODO TRADICIONAL
+
+/* const navbar = document.getElementsByClassName("navbar")
 const contenedor = document.getElementsByClassName("container")
-const inputNombre = document.getElementById("inputName")
-const formulario = document.getElementsByTagName("form")
+const encabezados = document.getElementsByTagName("h2")
+const identificador = document.getElementById("inputName")
 
-console.log(formulario) */
+console.log(identificador) */
 
-// METODO MODERNO => id, class, tags
+// METODOS MODERNOS
 
-/* const navbar = document.querySelector(".navbar")
-const contenedor = document.querySelectorAll(".container")
-const inputNombre = document.querySelector("#inputName")
-const formulario = document.querySelector("form")
-console.log(formulario) */
+/* const navegacion = document.querySelector(".navbar") // clases
+const titulos = document.querySelectorAll("h2") // todas las etiquetas
+const idForm = document.querySelector("#inputEmail") // id
 
-/* const encabezado = document.querySelector("h2").textContent= "Nuevo encabezado desde Javascript"
-console.log(encabezado) */
+console.log(idForm) */
 
-// Eliminar un elemento del DOM
 
-/* const textDelete = document.querySelector("h5")
-textDelete.remove()
+// 1) capturar el elemento del dom
 
-console.log(textDelete) */
+/* const encabezado = document.querySelector("h2").textContent = "Mi lista de productos" */
 
-// Agregar elementos en el DOM
 
-/* const textAdded = document.createElement("h5")
-textAdded.innerHTML = "<h5>Texto agregado</h5>"
-document.body.appendChild(textAdded)
+//Modificar CSS
+/* const encabezado = document.querySelector("h2")
 
-console.log(textAdded) */
+encabezado.style.backgroundColor = "red"
+*/
+// Eliminar un elemento del dom
 
-/* const listaVacia = document.querySelector(".lista-vacia")
+/* const elementoAEliminar = document.querySelector("h5")
+elementoAEliminar.remove()
 
-let otrosCursos=["Desarrollo Web","Javascript","ReactJs","NodeJs"]
 
-for (let curso of otrosCursos){
-    let listado = document.createElement("li")
-    listado.innerHTML= curso
+console.log(elementoAEliminar)
+ */
+
+
+// AGREGAR ELEMENTOS EN EL DOM
+
+/* const agregarElemento = document.createElement("div")
+
+agregarElemento.innerHTML = "<h5>Parrafo agregado</h5>"
+
+document.body.appendChild(agregarElemento) */
+
+
+
+/* const otrosCursos = [
+    "Desarrollo Web",
+    "javascript",
+    "ReactJs",
+    "NodeJs"
+]
+
+for (let curso of otrosCursos) {
+    let listado = document.createElement("ul")
+    listado.innerHTML = curso
     listaVacia.appendChild(listado)
 }
+*/
+// console.log(otrosCursos)
+// console.log(listaVacia)
 
-console.log(listaVacia) */
+
+
+/* const listaVacia = document.querySelector("#lista-vacia")
 
 let cursos = [
-    {id:1 , titulo:"Desarrollo Web" , precio:1000},
-    {id:2 , titulo: "Javascript (con carpi y el tio Omar)" , precio:9000},
-    {id:3 , titulo:"React Js(Omar)" , precio: 1000},
-    {id:4 , titulo: "Node Js", precio: 6000}
+
+    { id: 2, titulo: "Javascript", precio: 3000 },
+    { id: 3, titulo: "ReactJS", precio: 5000 }
+
 ]
 
 
-for (let curso of cursos){
+console.log(cursos)
+
+
+for (h of cursos) {
     let contenedor = document.createElement("div")
-    contenedor.innerHTML=
-    `
-
-    <div class="card border-dark mb-3" style="max-width: 20rem;">
-    <div class="card-header">${curso.titulo}</div>
-    <div class="card-body">
-        <p class="card-text"> $ ${curso.precio}</p>
-            <button type="button" class="btn btn-dark">Agregar al carrito</button>
-        </div>
-    </div>
-
-    
-    `
-
+    contenedor.innerHTML =
+        `
+    <h3>Curso: ${h.titulo}</h3>
+    <p>Precio: $ ${h.precio}</p>
+    <button type="button" class="btn btn-danger">Agregar al carrito</button>
+    `;
     document.body.appendChild(contenedor)
 }
+ */
+
+
+//Reduce para los prod del carrito
+// productos.reduce(()=>{
+
+// },0)
+// .reduce
+
+//Busqueda de productos por precio .filter
+
+//Array de objetos
+
+//calcular el costo del envio
+
+/* function calcularEnvio (){
+    return total + 900
+} */
+
+// Validar nombre de usuario
+
+/* if(){
+
+}else{
+
+} */
+
+// Capturar el elemento
+// const navegacion = document.querySelector(".navbar")
+
+// llamar a la variable
+// Asigno el escuchador, el evento y la funcion
+/* navegacion.addEventListener("click", () => {
+    console.log("Me has dado click")
+}) */
+
+/* navegacion.addEventListener("mouseout", () => {
+    console.log("Mouse Saliendo")
+}) */
+
+/* navegacion.addEventListener("dblclick", () => {
+    console.log("Me has dado dos click")
+}) */
+
+
+// const inputName = document.querySelector("#inputName")
+
+/* inputName.addEventListener("keydown", () => {
+    console.log("Escribiendo...")
+})
+ */
+/* inputName.addEventListener("blur", () => {
+    console.log("Validando informacion...")
+}) */
+
+/* inputName.addEventListener("input", () => {
+    console.log("Dentro del input...")
+}) */
+
+// const inputName = document.querySelector("#inputName")
+
+/* inputName.addEventListener("input", (e) => {
+    console.log(e.target.value)
+}) */
+
+
+//Validacion para que el input no quede vacío
+/* inputName.addEventListener("input", (e) => {
+    if (e.target.value === "") {
+        console.log("Falló la validación")
+    }
+}) */
+
+
+
+
+/* const formulario = document.querySelector("#formulario")
+const inputName = document.querySelector("#inputName")
+const inputEmail = document.querySelector("#inputEmail")
+
+formulario.addEventListener("submit", validarFormulario)
+
+
+function validarFormulario(e) {
+    e.preventDefault()
+
+    console.log(`Nombre del usuario ${inputName.value}, correo electrónico ${inputEmail.value}`)
+} */
+
+const cardHeader = document.querySelector(".card-header")
+const cardText = document.querySelector(".card-text")
+const btn = document.querySelector("#btnInfo")
+
+btn.addEventListener("click", () => {
+    Swal.fire({
+        title: 'Submit your Github username',
+        input: 'text',
+        inputAttributes: {
+            autocapitalize: 'off'
+        },
+        showCancelButton: true,
+        confirmButtonText: 'Look up',
+        showLoaderOnConfirm: true,
+        preConfirm: (login) => {
+            return fetch(`//api.github.com/users/${login}`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(response.statusText)
+                    }
+                    return response.json()
+                })
+                .catch(error => {
+                    Swal.showValidationMessage(
+                        `Request failed: ${error}`
+                    )
+                })
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: `${result.value.login}'s avatar`,
+                imageUrl: result.value.avatar_url
+            })
+        }
+    })
+})
+
+
+/* console.log(cardHeader)
+console.log(cardText)
+console.log(btn) */
